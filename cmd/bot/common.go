6c6d0488a6fc26e0011870d60d85fa0bfb61145b
Paul Karpukhin <ruberoid55@gmail.com>
2025-07-24 09:21:03 +0300

    removed unsued method

--- cmd/bot/common.go
+++ cmd/bot/common.go
@@ -1,19 +1,8 @@
 package main
 
 import (
-	"spacedrepetitiongo/flashcard"
-
 	"strings"
-
-	"github.com/jmoiron/sqlx"
 )
-
-func sendNewFlashCardToTelegramIfExists(db sqlx.DB, boxId string, tableName string, sendToTelegram func(flashcard.Flashcard)) {
-	if flashcard.CountInDb(db, boxId, tableName) > 0 {
-		flashCard := flashcard.GetFormBox(db, boxId, tableName)
-		sendToTelegram(flashCard)
-	}
-}
 
 func fetchValue(payload string) string {
 	return strings.Split(payload, "=")[1]
