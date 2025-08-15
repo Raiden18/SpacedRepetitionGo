cd586cc6ac800e7cdcc89aeb13e98e138c815498
Paul Karpukhin <ruberoid55@gmail.com>
2025-08-15 20:16:07 +0300

    base64

--- flashcard/flashcard.go
+++ flashcard/flashcard.go
@@ -1,7 +1,12 @@
 package flashcard
 
 import (
+	"bytes"
+	"encoding/base64"
 	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
 	"io"
 	"log"
 	"os"
@@ -55,9 +60,7 @@ func GenerateFromGPT(
 		),
 	)
 
-	log.Printf(urlImage)
-	downloadImage(
-		resty.New(),
+	convertBase64ToImage(
 		urlImage,
 		"/root/repetition/images/",
 		"gpt_generated",
@@ -248,4 +251,77 @@ func findFileByNameWithoutExt(folderPath, baseName string) (string, error) {
 	}
 
 	return "", fmt.Errorf("file %q not found in %q", baseName, folderPath)
+}
+
+func convertBase64ToImage(base64Str, outputFolder, baseFilename string) {
+	if idx := strings.Index(base64Str, "base64,"); idx != -1 {
+		base64Str = base64Str[idx+7:]
+	}
+
+	decoded, err := base64.StdEncoding.DecodeString(base64Str)
+
+	if err != nil {
+		log.Println("Could not decode base64 image.", err)
+		return
+	}
+
+	img, format, err := image.Decode(bytes.NewReader(decoded))
+	if err != nil {
+		log.Println("Could not decode image.", err)
+		return
+	}
+
+	var ext string
+	switch format {
+	case "jpeg":
+		ext = ".jpg"
+	case "png":
+		ext = ".png"
+	case "gif":
+		ext = ".gif"
+	default:
+		log.Println("Could not decode base64 image. Unsupported image formag: " + format)
+	}
+
+	outputPath := outputFolder + "/" + baseFilename + ext
+
+	savingToFile := map[string]func(){
+		"jpeg": func() { saveJPEG(outputPath, img) },
+		"png":  func() { savePNG(outputPath, img) },
+		"gif":  func() { saveGif(outputPath, decoded) },
+	}
+
+	save := savingToFile[format]
+	save()
+}
+
+func savePNG(path string, img image.Image) {
+	saveImage(path, img, png.Encode)
+}
+
+func saveGif(path string, decoded []byte) {
+	os.WriteFile(path, decoded, 0644)
+}
+
+func saveJPEG(path string, img image.Image) {
+	error := saveImage(
+		path,
+		img,
+		func(w io.Writer, image image.Image) error {
+			return jpeg.Encode(w, image, nil)
+		},
+	)
+	if error != nil {
+		log.Println("Could not save image in file. filePath : " + path)
+	}
+}
+
+func saveImage(path string, image image.Image, encode func(w io.Writer, image image.Image) error) error {
+	outFile, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	return encode(outFile, image)
 }
